00da4cf88013446929551fb7aab525c693922d5b
maheshinder19 <mahesh.inder85@gmail.com>
2019-04-08 14:55:28 +0530

    middleware complete

--- middlewares/middleware/src/index.js
+++ middlewares/middleware/src/index.js
@@ -1,6 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
 import { createStore, applyMiddleware } from "redux";
 
-const product = [];
+ReactDOM.render(<App />, document.getElementById("root"));
+
+const product = []; //initial state
+
+//reducer
 
 const products = (state = product, action) => {
   switch (action.type) {
@@ -14,6 +22,8 @@ const products = (state = product, action) => {
   }
 };
 
+//middleware that handles both arrays and objects
+
 const handleProducts = store => next => action => {
   if (Array.isArray(action)) {
     action.map(product =>
@@ -26,16 +36,20 @@ const handleProducts = store => next => action => {
 
 const middleware = applyMiddleware(handleProducts);
 
+//store
 const store = createStore(products, middleware);
 store.subscribe(() => {
   console.log("Store has changed", store.getState());
 });
 
+//action_creator
 const addProduct = product => ({ type: "ADD_PRODUCT", product });
 
+//actions fired
 store.dispatch(addProduct({ id: 1, name: "Red bull" }));
 store.dispatch(addProduct({ id: 2, name: "Coke" }));
 
+//array_action fired
 store.dispatch([
   {
     id: 3,
